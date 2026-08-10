3420b9d10b12b07ea47d49e976be1ee133c74000
Aarushi <aarushi.vv28@gmail.com>
2026-08-10 20:51:45 +0530

    Time: 511 ms (5.12%), Space: 92.9 MB (13.68%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,25 @@
+class StockSpanner {
+public:
+    vector<int> arr;
+
+    StockSpanner() {
+        // nothing needed
+    }
+
+    int next(int price) {
+
+        arr.push_back(price);
+
+        int cnt = 1;
+
+        for(int i = arr.size() - 2; i >= 0; i--) {
+
+            if(arr[i] <= price)
+                cnt++;
+            else
+                break;
+        }
+
+        return cnt;
+    }
+};
