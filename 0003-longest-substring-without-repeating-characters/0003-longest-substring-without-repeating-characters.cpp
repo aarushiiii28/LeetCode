dbdc9fad4645026255f0e04e232672c4b370278c
Aarushi <aarushi.vv28@gmail.com>
2026-08-12 14:56:32 +0530

    Time: 7 ms (68.98%), Space: 19 MB (23.9%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,23 +3,26 @@ public:
     int lengthOfLongestSubstring(string s) {
 
         int n = s.size();
-        int l = 0;
-        int r = 0;
+        int l = 0, r = 0;
 
-        unordered_map<char, int> mpp;
+        vector<int> mpp(256, -1);
         int max_length = 0;
 
-        while(r<n){
+        while (r < n) {
 
-            if(mpp.find(s[r]) != mpp.end() && mpp[s[r]] >= l){
+           
+            if (mpp[s[r]] >= l) {
                 l = mpp[s[r]] + 1;
             }
 
             max_length = max(max_length, r - l + 1);
 
+        
             mpp[s[r]] = r;
+
             r++;
         }
-    return max_length;
+
+        return max_length;
     }
 };
